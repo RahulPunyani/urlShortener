1b62080b741fb99af9f81d7e51ea5ce3b39e13c3
Chetan Punyani <chetanpunyani78@gmail.com>
2025-08-05 01:19:43 +0530

    Database updates
    Switched from a local database to a live hosted cloud database

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/urlShortener");
+    await mongoose.connect("mongodb+srv://urlshortner_api:vVhyijd0e1rhSf0X@database-node.tegul.mongodb.net/urlshortener?retryWrites=true&w=majority&appName=database-node");
     console.log('✅ MongoDB connected');
   } catch (err) {
     console.error('❌ MongoDB connection failed:', err);
