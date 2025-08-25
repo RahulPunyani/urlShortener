dbb7cd4bd0204ef772fb9d791e35d6d19177e678
Rahul Punyani <punyanirahul17@gmail.com>
2025-08-25 21:37:46 +0200

    Adding frontend to cors enabled list

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://url-shortener-4b4c.vercel.app"], credentials: true }));
 
 app.use(express.json());
 connectDB();
